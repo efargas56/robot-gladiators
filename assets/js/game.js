56a239b50c695e86702eb9b502f320292f12bad1
ef56 <efargas56@gmail.com>
2022-03-24 04:42:45 -0400

    added else and if statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerHealth, playerAttack);
@@ -13,6 +14,9 @@ var fight = function(){
 // Alert players that they are starting the round
 
 window.alert("Welcome to Robot Gladiators!");
+var promptFight = window.prompt("would you like to attack? or skip turn? Enter 'FIGHT' or 'SKIP' to choose.")
+// if player choses to fight, then fight
+if  (promptFight === "fight" || promptFight === "FIGHT") {
 //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 enemyHealth = enemyHealth - playerAttack;
 // Log a resulting message to the console so we know that it worked.
@@ -40,4 +44,25 @@ else {
   window.alert(playerName + " still has " + playerHealth + " health left.");
 }
 }
+else if (promptFight === "SKIP" || promptFight === "skip"){
+// confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  if(confirmSkip) {
+  window.alert(playerName + " has chosen to skip the fight!");
+// subtract money from playerMoney for skipping
+  playerMoney - 2;
+  }
+    // if no (false), ask question again by running fight() again
+  else {
+      fight();
+  }
+}
+
+
+
+else {
+  window.alert("you need to choose a valid option. Try Again!")
+}
+}
+
  fight();
