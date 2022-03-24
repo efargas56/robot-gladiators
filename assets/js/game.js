c35c01e1d2b0b2f0384b8caf422e2c877a897958
ef56 <efargas56@gmail.com>
2022-03-24 01:17:34 -0400

    edited javascript

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("Our robot's name is " + playerName);
+ //function fight() {
+  //  window.alert("The fight has begun!");
+//}
+// fight();
